02db04ab086b361a3af62c34ddc8e97e82b67701
S_H_Xu <3544480780@qq.com>
2021-11-25 23:16:39 +0800

    self study C&C++

---
+++ test_21_1125/test1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+T maxn(const T *);
+
+int main()
+{
+	double arr[5] = {};
+
+	for(int i = 0; i < 5; i++)
+		cin >> arr[i];
+	double max = maxn(arr);
+	cout << "Max = " << max << endl;
+
+	return 0;
+}
+
+template <class T>
+T maxn(const T* arr)
+{
+	T temp = arr[0];
+	for(int i = 1; i < 5; i++)
+		temp = temp > arr[i] ? temp : arr[i];
+
+	return temp;	
+}
