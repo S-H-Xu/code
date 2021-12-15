0f591102a025376a566a1b20f9786e05afdb5445
S_H_Xu <3544480780@qq.com>
2021-12-15 23:15:40 +0800

    self study C&C++

---
+++ test_21_1215/14.7_1/wine.h
@@ -0,0 +1,13 @@
+//wine.h
+#ifndef __WINE_H__
+#define __WINE_H__
+#include <iostream>
+#include <string>
+#include <valarry>
+
+typedef std::valarray<int> ArrayInt;
+typedef std::Pair<ArrayInt, ArrayInt> PairArray;
+
+
+
+#endif
