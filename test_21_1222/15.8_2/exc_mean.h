e81e3cd24a8e11013309cc4b6d280a80f3225676
S_H_Xu <3544480780@qq.com>
2021-12-22 22:18:11 +0800

    \0

---
+++ test_21_1222/15.8_2/exc_mean.h
@@ -0,0 +1,35 @@
+//exc_mean.h
+#ifndef __EXC_MEAN_H__
+#define __EXC_MEAN_H__
+
+class bad_hmean
+{
+private:
+	double v1;
+	double v2;
+public:
+	bad_hmean(double a = 0, double b = 0) :v1(a), v2(b) {}
+	void mesg();
+};
+
+inline void bad_hmean::mesg()
+{
+	std::cout << "hmean(" << v1 << ", " << v2 << "): "
+		<< "invalid arguments: a = -b\n";
+}
+
+class bad_gmean
+{
+public:
+	double v1;
+	double v2;
+	bad_gmean(double a = 0, double b = 0) : v1(a), v2(b) {}
+	const char* mesg();
+};
+
+inline const char* bad_gmean::mesg()
+{
+	return "gmean() arguments should be >=0\n";
+}
+
+#endif
