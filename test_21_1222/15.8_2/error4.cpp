ebf33adcf331c3476a7e5a6f348b26b6fa07ee06
S_H_Xu <3544480780@qq.com>
2021-12-27 20:16:55 +0800

    self study C&C++

--- test_21_1222/15.8_2/error4.cpp
+++ test_21_1222/15.8_2/error4.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cmath>
 #include "exc_mean.h"
+#include <stdexcept>
 
 double hmean(double a, double b);
 double gmean(double a, double b);
@@ -29,13 +30,13 @@ int main()
 		}//end of try block
 		catch (bad_hmean& bg)//start of catch block
 		{
-			bg.mesg();
+			cout << bg.what() << endl;
 			cout << "Try again.\n";
 			continue;
 		}
 		catch (bad_gmean& hg)
 		{
-			cout << hg.mesg();
+			cout << hg.what() << endl;
 			cout << "Valus used: " << hg.v1 << ", "
 				<< hg.v2 << endl;
 			cout << "Sorry, you don't get to play any more.\n";
@@ -50,14 +51,14 @@ int main()
 
 double hmean(double a, double b)
 {
-	if (a == b)
+	if (a == -b)
 		throw bad_hmean(a, b);
 	return 2.0 * a * b / (a + b);
 }
 
 double gmean(double a, double b)
 {
-	if (a < b || b < 0)
+	if (a < 0 || b < 0)
 		throw bad_gmean(a, b);
 	return std::sqrt(a * b);
 }
--- test_21_1222/15.8_2/exc_mean.h
+++ test_21_1222/15.8_2/exc_mean.h
@@ -1,35 +1,42 @@
 //exc_mean.h
 #ifndef __EXC_MEAN_H__
 #define __EXC_MEAN_H__
+#include <stdexcept>
 
-class bad_hmean
+class bad_hmean : public std::logic_error
 {
 private:
 	double v1;
 	double v2;
 public:
-	bad_hmean(double a = 0, double b = 0) :v1(a), v2(b) {}
-	void mesg();
+	bad_hmean(double a = 0, double b = 0) :
+	       	logic_error("invalid arguments: a = -b"), v1(a), v2(b) {}
+	//void mesg();
 };
 
-inline void bad_hmean::mesg()
+/*
+inline void bad_hmean::what()
 {
 	std::cout << "hmean(" << v1 << ", " << v2 << "): "
 		<< "invalid arguments: a = -b\n";
 }
+*/
 
-class bad_gmean
+class bad_gmean : public std::logic_error
 {
 public:
 	double v1;
 	double v2;
-	bad_gmean(double a = 0, double b = 0) : v1(a), v2(b) {}
-	const char* mesg();
+	bad_gmean(double a = 0, double b = 0) :
+	       logic_error("gmean() arguments should be >= 0"), v1(a), v2(b) {}
+	//const char* mesg();
 };
 
-inline const char* bad_gmean::mesg()
+/*
+inline const char* bad_gmean::what()
 {
 	return "gmean() arguments should be >=0\n";
 }
+*/
 
 #endif
